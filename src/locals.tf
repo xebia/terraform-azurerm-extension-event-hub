a5b41d3e9a8df78576d3791934d409f9d7388be1
Marcin Słowikowski <59068696+msl0@users.noreply.github.com>
2025-12-15 16:31:21 +0100

    Update azurerm_eventhub resource to use inline for_each instead of local variable

--- src/locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  eventhubs_map = { for eh in var.eventhubs : eh.name => eh }
-}
--- src/main.tf
+++ src/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_eventhub" "eventhub" {
   provider = azurerm.eventhub
 
-  for_each = local.eventhubs_map
+  for_each = { for eh in var.eventhubs : eh.name => eh }
 
   namespace_id      = var.event_hub_namespace_id
   name              = each.value.name
@@ -11,35 +11,40 @@ resource "azurerm_eventhub" "eventhub" {
 }
 
 resource "azurerm_role_assignment" "sp_event_hub_uac" {
-  for_each             = azurerm_eventhub.eventhub
+  for_each = azurerm_eventhub.eventhub
+
   principal_id         = var.service_principal_object_id
   role_definition_name = "User Access Administrator"
   scope                = each.value.id
 }
 
 resource "azurerm_role_assignment" "owner_event_hub_group_data_owner" {
-  for_each             = azurerm_eventhub.eventhub
+  for_each = azurerm_eventhub.eventhub
+
   principal_id         = var.owner_group_id
   role_definition_name = "Azure Event Hubs Data Owner"
   scope                = each.value.id
 }
 
 resource "azurerm_role_assignment" "owner_group_event_hub_owner" {
-  for_each             = azurerm_eventhub.eventhub
+  for_each = azurerm_eventhub.eventhub
+
   principal_id         = var.owner_group_id
   role_definition_name = "Owner"
   scope                = each.value.id
 }
 
 resource "azurerm_role_assignment" "contributor_group_event_hub_contributor" {
-  for_each             = azurerm_eventhub.eventhub
+  for_each = azurerm_eventhub.eventhub
+
   principal_id         = var.contributor_group_id
   role_definition_name = "Contributor"
   scope                = each.value.id
 }
 
 resource "azurerm_role_assignment" "reader_group_event_hub_reader" {
-  for_each             = azurerm_eventhub.eventhub
+  for_each = azurerm_eventhub.eventhub
+
   principal_id         = var.reader_group_id
   role_definition_name = "Reader"
   scope                = each.value.id
